d8593c1849a26b319a0fc321ddbcbf71840ee404
krishnaggk <krishna.ggk@gmail.com>
2010-02-25 23:02:23 +0530

    Added ActionManager and SingletonManager classes.
    * ActionManager encapuslate action specific functionalities.
    * SingletonManger acts as container for all singleton classes which will
    be introduced sooner.

---
+++ qucs/singletonmanager.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ * Copyright (C) 2010 by Gopala Krishna A <krishna.ggk@gmail.com>          *
+ *                                                                         *
+ * This is free software; you can redistribute it and/or modify            *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 2, or (at your option)     *
+ * any later version.                                                      *
+ *                                                                         *
+ * This software is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this package; see the file COPYING.  If not, write to        *
+ * the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,   *
+ * Boston, MA 02110-1301, USA.                                             *
+ ***************************************************************************/
+
+#ifndef SINGLETONMANAGER_H
+#define SINGLETONMANAGER_H
+
+#include <QObject>
+
+class ActionManager;
+
+class SingletonManager : public QObject
+{
+public:
+    ~SingletonManager();
+
+    ActionManager* actionManager();
+
+    static SingletonManager* instance();
+
+private:
+    SingletonManager(QObject *parent = 0);
+    ActionManager *m_actionManager;
+};
+
+#endif // SINGLETONMANAGER_H
