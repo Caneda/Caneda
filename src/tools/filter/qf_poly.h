1e4cdb91416ea114c58b1aac8e33f2fb479631a3
Pablo Daniel Pareja Obregon <parejaobregon@gmail.com>
2012-07-17 11:14:29 -0300

    Clean up like changes.

--- src/tools/filter/qf_poly.h
+++ src/tools/filter/qf_poly.h
@@ -22,7 +22,35 @@
 
 #define qf_double_t long double
 
-#include "qf_matrix.h"
+
+class qf_matrix
+{
+ public:
+  // constructor
+  qf_matrix (unsigned int d) {
+    data = (qf_double_t *) calloc (d * d, sizeof (qf_double_t));
+    n = d;
+  }
+
+  // destructor
+  ~qf_matrix () {
+    free (data);
+  }
+
+  // accessor operators
+  qf_double_t operator () (int r, int c) const {
+    return data[r * n + c];
+  }
+  qf_double_t & operator () (int r, int c) {
+    return data[r * n + c];
+  }
+
+  // size of matrix
+  unsigned int n;
+
+ private:
+  qf_double_t * data;
+};
 
 // A polynom can be described either by a product of monoms equal to
 // (x - r[i]) where r[i] is the ith root and a constant factor, or by
